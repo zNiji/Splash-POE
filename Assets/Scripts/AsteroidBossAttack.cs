a3369f3b186344211f2fe54905c9aac998258e9d
LiamFullard <liamfullard23@gmail.com>
2025-06-16 16:05:44 +0200

    Added colliders and other assets
    - Added colliders to all Level 2 prefabs
    - Added textures for The Moon boss
    - Added The Moon boss attack (asteroid prefab and script)

---
+++ Assets/Scripts/AsteroidBossAttack.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class AsteroidBossAttack : MonoBehaviour
+{
+    [SerializeField] float speed = 0;
+
+    private void Update()
+    {
+
+        Vector3 forward = transform.forward;
+
+        Vector3 direction = forward * speed;
+
+        transform.position += direction * Time.deltaTime;
+
+        Destroy(gameObject, 5f);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<ControlSplash>() != null)
+        {
+            ControlSplash playerController = collision.gameObject.GetComponent<ControlSplash>();
+        }
+
+    }
+}
